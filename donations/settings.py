97f3686906c84a14ddc2c54f750147fefc62f954
ErnestoTSantos <ernesto.terra2003@gmail.com>
2024-05-14 20:39:11 -0300

    fix: adjust hosts

--- donations/settings.py
+++ donations/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = ["localhost", "https://social-food-ba6618ab911b.herokuapp.com/"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
